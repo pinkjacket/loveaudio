15c9ec379b610f8d31363b5dd78dd64725785a8e
James Donlan <jacketlizard@live.com>
2019-06-22 11:13:13 -0700

    sound effect now plays over song when space bar is pressed

--- main.lua
+++ main.lua
@@ -2,4 +2,12 @@ function love.load()
   song = love.audio.newSource("sounds/song.ogg", "stream")
   song:setLooping(true)
   song:play()
+  
+  sfx = love.audio.newSource("sounds/sfx.ogg", "static")
+end
+
+function love.keypressed(key)
+  if key == "space" then
+    sfx:play()
+  end
 end
